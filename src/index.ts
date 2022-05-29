245378d0486decee78aa3eec1ee5be88e139b5a9
A90G <104040612+A90G@users.noreply.github.com>
2022-05-29 19:19:15 -0300

    commit sunday advanced

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import "./styles.css";
+/*Diseñar un algoritmo que recorra las
+butacas de una sala de cine y determine
+cuántas butacas desocupadas hay
+• Suponga que para modelar este
+problema, se utiliza un arreglo con
+valores lógicos
+• La presencia de un valor verdadero (true) en
+el arreglo indica que la butaca está ocupada
+• La presencia de un valor falso (false) en el
+arreglo indica que la butaca está desocupada*/
+let dimensionButaca: number = Number(prompt("ingrese el nùmero de butacas"));
+let butaca: boolean[] = new Array(dimensionButaca);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+let resultadoTrue: number = 0;
+let resultadoFalse: number = 0;
+let indice: number = 0;
+
+for (indice = 0; indice < butaca.length; indice++) {
+  if (Math.random() < 0.3) {
+    resultadoTrue++;
+    console.log("Butaca ocupada");
+  } else {
+    resultadoFalse++;
+    console.log("Butaca libre");
+  }
+}
+console.log(`Hay ${resultadoTrue} butacas ocupadas`);
+
+console.log(`Hay ${resultadoFalse} butacas libres`);
